92a45817b116cf1193688687a757a532456ccee5
Francisco Thiago <fthiagomedeiros@gmail.com>
2020-03-30 01:41:48 -0300

    feature/s3 signed url (#10)
    * feature/s3-signed-url (#9)
    
    * S3 BucketPolicy and S3 BucketImages
    
    * SignedURL and including react client compatible
    
    * ApiGatewayId

---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = 'https://ApiGatewayId.execute-api.us-east-1.amazonaws.com/dev';
+
+export const authConfig = {
+  domain: '...',
+  clientId: '...',
+  callbackUrl: 'http://localhost:3000/callback'
+};
